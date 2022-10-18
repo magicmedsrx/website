0e7262de043b51a5a0a2b8027a7683ca29712753
magicmedsrx <magicmedsrx@gmail.com>
2022-10-18 14:07:25 -0400

    UPDATED INCLUDING J&J AND REFILL PROCESS

--- js/include.js
+++ js/include.js
@@ -29,7 +29,7 @@ function removeCookie() {
 function checkCookie() {
     if (getCookie('Authentication') == 'true') {
         document.getElementById("user-input").setAttribute('style', 'display: none');
-        document.getElementById("iframe").innerHTML = '<iframe id="iframe" src="MAGIC FULL 10.11.22.pdf#toolbar=0"></iframe>';
+        document.getElementById("iframe").innerHTML = '<iframe id="iframe" src="MAGIC FULL 10.18.22.pdf#toolbar=0"></iframe>';
         document.getElementById("log-out").setAttribute('style', 'display: block');
     }
 }
