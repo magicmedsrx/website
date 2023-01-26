b80bd31b9237a2162a8583b6a0f05e1971f655b8
magicmedsrx <magicmedsrx@gmail.com>
2023-01-26 13:29:23 -0500

    Updated PDF to remove Johnson & Johnson and updated re-enrollment information

--- js/include.js
+++ js/include.js
@@ -1,5 +1,5 @@
 // when updating the pdf names increment the version number in institution.html, important!
-const INSTITUTION_PDF_NAME = "MAGICFULL01.25.23.pdf";
+const INSTITUTION_PDF_NAME = "MAGICFULL01.26.23.pdf";
 const PATIENT_PDF_NAME = "MAGICSHORT01.17.23.pdf";
 
 let IS_MOBILE = false;
