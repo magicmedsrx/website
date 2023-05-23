c005c85d4d75312f7b937f023498f7457e6ecc8a
magicmedsrx <magicmedsrx@gmail.com>
2023-05-23 09:04:25 -0400

    Updated institutional version

--- js/include.js
+++ js/include.js
@@ -1,5 +1,5 @@
 // when updating the pdf names increment the version number in institution.html, important!
-const INSTITUTION_PDF_NAME = "MAGICFULL05.12.23.pdf";
+const INSTITUTION_PDF_NAME = "MAGICFULL05.22.23.pdf";
 const PATIENT_PDF_NAME = "MAGICSHORT05.22.23.pdf";
 
 let IS_MOBILE = false;
