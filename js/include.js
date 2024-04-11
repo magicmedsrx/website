d6e513414ac344ce72bc0c196f9e073040bbb49a
magicmedsrx <magicmedsrx@gmail.com>
2024-04-11 17:54:12 -0400

    Final updates April. AMGEN income

--- js/include.js
+++ js/include.js
@@ -1,5 +1,5 @@
 // when updating the pdf names increment the version number in institution.html, important!
-const INSTITUTION_PDF_NAME = "MAGICFULL03.20.24.pdf";
+const INSTITUTION_PDF_NAME = "MAGICFULL04.11.24.pdf";
 const PATIENT_PDF_NAME = "MAGICSHORT05.22.23.pdf";
 
 let IS_MOBILE = false;
