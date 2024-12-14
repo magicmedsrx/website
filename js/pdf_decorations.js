0540bae768dd6554e90dd4c165a2bc87991fd345
magicmedsrx <magicmedsrx@gmail.com>
2024-12-14 08:26:42 -0500

    Updated standard and enhanced on Med part D page

--- js/pdf_decorations.js
+++ js/pdf_decorations.js
@@ -6,4 +6,4 @@ const PATIENT_PDF_NAME = 'MAGICSHORT05.22.23.pdf';
 
 // medicare.html
 const FL_MEDICARE_ADVANTAGE = 'fl_advantage_10_21_24.pdf';
-const FL_MEDICARE_PART_D = 'fl_part_d_10_21_24.pdf';
+const FL_MEDICARE_PART_D = 'PartD12.13.24.pdf';
