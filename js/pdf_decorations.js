a4a10b180099ad2dcc7fef9379553eb275932f6a
magicmedsrx <magicmedsrx@gmail.com>
2025-10-21 15:15:51 -0400

    Updated Humana Premier Rx premium monthly

--- js/pdf_decorations.js
+++ js/pdf_decorations.js
@@ -8,5 +8,5 @@ const PATIENT_PDF_NAME = 'MAGICSHORT05.22.23.pdf';
 const FL_MEDICARE_ADVANTAGE = 'fl_advantage_10_21_24.pdf';
 const FL_MEDICARE_PART_D = 'PartD12.13.24.pdf';
 const medicareextrahelp = 'Extrahelp07.23.25.pdf';
-const pdppreview = 'flpdp10.03.25.pdf';
+const pdppreview = 'flpdp10.21.25.pdf';
 const mapdpreview = 'flmapd10.03.25.pdf';
