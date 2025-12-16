fb01b64b9fe73438b0e79dd5dd85c4d163746758
magicmedsrx <magicmedsrx@gmail.com>
2025-12-16 10:59:40 -0500

    Updated Eliquis and BMS

--- js/pdf_decorations.js
+++ js/pdf_decorations.js
@@ -1,7 +1,7 @@
 // when updating the pdf names increment the version numbers
 
 // institution.html
-const INSTITUTION_PDF_NAME = 'MAGICFULL11.11.25.pdf';
+const INSTITUTION_PDF_NAME = 'MAGICFULL12.16.25.pdf';
 const PATIENT_PDF_NAME = 'MAGICSHORT05.22.23.pdf';
 
 // medicare.html
