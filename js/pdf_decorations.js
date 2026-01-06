b93d220b6ea7315804559d677d805b3fc21829cf
magicmedsrx <magicmedsrx@gmail.com>
2026-01-06 16:23:20 -0500

    Add J&J Pharmacy and got rid of Medicare 2025 information

--- js/pdf_decorations.js
+++ js/pdf_decorations.js
@@ -1,7 +1,7 @@
 // when updating the pdf names increment the version numbers
 
 // institution.html
-const INSTITUTION_PDF_NAME = 'MAGICFULL12.16.25.pdf';
+const INSTITUTION_PDF_NAME = 'MAGICFULL01.06.26.pdf';
 const PATIENT_PDF_NAME = 'MAGICSHORT05.22.23.pdf';
 
 // medicare.html
